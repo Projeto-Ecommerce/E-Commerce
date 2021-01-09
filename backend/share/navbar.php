24b7ff6341f7686d8a4f6e01eba7508a1b7e4405
GrupoTHZ <70438638+GrupoTHZ@users.noreply.github.com>
2021-01-09 15:08:06 -0300

    backend

---
+++ backend/share/navbar.php
@@ -0,0 +1,31 @@
+<div id="navbar">
+      <div class="dropdown">
+      <button href="javascript:void(0)" class="dropbtn">CHURRASQUEIRAS RESIDENCIAIS <i class="fa fa-caret-down"></i></button>
+        <div class="dropdown-content">
+          <a href="javascript:void(0)">Churrasqueira para Embutir a Carvão</a>
+          <a href="javascript:void(0)">Churrasqueira Portátil a Carvão</a>
+        </div>
+      </div> 
+      <div class="dorpdown">
+        <button href="javascript:void(0)" class="dropbtn">CHURRASQUEIRAS PROFISSIONAIS <i class="fa fa-caret-down"></i></button>
+        <div class="dropdown-content">
+          <a href="javascript:void(0)">Churrasqueira p/ Espetinho a Carvão para BalcãoChurrasqueira para Embutir a Carvão</a>
+          <a href="javascript:void(0)">Churrasqueira p/ Espetinho a Carvão Portátil</a>
+        </div>
+      </div>
+        <a href="javascript:void(0)">GRILL</a>
+        <div class="dropdown">
+            <button href="javascript:void(0)" class="dropbtn">ACESSÓRIOS <i class="fa fa-caret-down"></i></button>
+            <div class="dropdown-content">
+                <a href="javascript:void(0)">Grelha Argentina</a>
+                <a href="javascript:void(0)">Espeto</a>
+                <a href="javascript:void(0)">Grelha Moeda</a>
+                <a href="javascript:void(0)">Anti Chama</a>
+                <a href="javascript:void(0)">Pazinha Raspadora</a>
+                <a href="javascript:void(0)">Peças de Reposição</a>
+            </div>
+        </div>
+        <a href="javascript:void(0)">FOTOS E VIDEOS</a>
+        <a href="javascript:void(0)">EMPRESA</a>
+        <a href="javascript:void(0)">CONTATO</a>
+</div>
