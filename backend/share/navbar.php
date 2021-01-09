ad6c21e931925b3a9cf850748188ebd49b0f7d69
GrupoTHZ <70438638+GrupoTHZ@users.noreply.github.com>
2021-01-09 16:58:20 -0300

    navbar

--- backend/share/navbar.php
+++ backend/share/navbar.php
@@ -1,31 +1,68 @@
-<div id="navbar">
-      <div class="dropdown">
-      <button href="javascript:void(0)" class="dropbtn">CHURRASQUEIRAS RESIDENCIAIS <i class="fa fa-caret-down"></i></button>
-        <div class="dropdown-content">
-          <a href="javascript:void(0)">Churrasqueira para Embutir a Carvão</a>
-          <a href="javascript:void(0)">Churrasqueira Portátil a Carvão</a>
-        </div>
-      </div> 
-      <div class="dorpdown">
-        <button href="javascript:void(0)" class="dropbtn">CHURRASQUEIRAS PROFISSIONAIS <i class="fa fa-caret-down"></i></button>
-        <div class="dropdown-content">
-          <a href="javascript:void(0)">Churrasqueira p/ Espetinho a Carvão para BalcãoChurrasqueira para Embutir a Carvão</a>
-          <a href="javascript:void(0)">Churrasqueira p/ Espetinho a Carvão Portátil</a>
-        </div>
-      </div>
-        <a href="javascript:void(0)">GRILL</a>
-        <div class="dropdown">
-            <button href="javascript:void(0)" class="dropbtn">ACESSÓRIOS <i class="fa fa-caret-down"></i></button>
-            <div class="dropdown-content">
-                <a href="javascript:void(0)">Grelha Argentina</a>
-                <a href="javascript:void(0)">Espeto</a>
-                <a href="javascript:void(0)">Grelha Moeda</a>
-                <a href="javascript:void(0)">Anti Chama</a>
-                <a href="javascript:void(0)">Pazinha Raspadora</a>
-                <a href="javascript:void(0)">Peças de Reposição</a>
-            </div>
-        </div>
-        <a href="javascript:void(0)">FOTOS E VIDEOS</a>
-        <a href="javascript:void(0)">EMPRESA</a>
-        <a href="javascript:void(0)">CONTATO</a>
-</div>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="collapse navbar-collapse" id="main_nav">
+    <ul class="navbar-nav">
+      <li class="nav-item dropdown">
+        <a class="nav-link  dropdown-toggle" href="#" data-toggle="dropdown"> CHURRASQUEIRAS RESIDENCIAIS</a>
+        <ul class="dropdown-menu">
+          <li><a class="dropdown-item" href="#">Churrasqueira para Embutir a Carvão</a></li>
+          <li><a class="dropdown-item" href="#">Churrasqueira Portátil a Carvão</a></li>
+        </ul>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link  dropdown-toggle" href="#" data-toggle="dropdown"> CHURRASQUEIRAS PROFISSIONAIS</a>
+        <ul class="dropdown-menu">
+          <li><a class="dropdown-item" href="#">Churrasqueira p/ Espetinho a Carvão para Balcão</a></li>
+          <li><a class="dropdown-item" href="#">Churrasqueira p/ Espetinho a Carvão Portátil</a></li>
+        </ul>
+      </li>
+      <li class="nav-item"><a class="nav-link" href="#"> GRILL </a></li>
+      <li class="nav-item dropdown">
+        <a class="nav-link  dropdown-toggle" href="#" data-toggle="dropdown"> ACESSÓRIOS </a>
+        <ul class="dropdown-menu">
+          <li><a class="dropdown-item" href="#">Grelha Argentina</a></li>
+          <li><a class="dropdown-item" href="#">Espeto</a></li>
+          <li><a class="dropdown-item" href="#">Grelha Moeda</a></li>
+          <li><a class="dropdown-item" href="#">Anti Chama</a></li>
+          <li><a class="dropdown-item" href="#">Pazinha Raspadora</a></li>
+          <li><a class="dropdown-item" href="#">Peças de Reposição</a></li>
+        </ul>
+      </li>
+      <li class="nav-item"><a class="nav-link" href="#"> FOTOS E VIDEOS </a></li>
+      <li class="nav-item"><a class="nav-link" href="#"> EMPRESA </a></li>
+      <li class="nav-item"><a class="nav-link" href="#"> CONTATO </a></li>
+    </ul>
+  </div> <!-- navbar-collapse.// -->
+</nav>
+
+<!-- 
+<div class="navbar">
+  <div class="dropdown">
+    <button href="javascript:void(0)" class="dropbtn">CHURRASQUEIRAS RESIDENCIAIS <i class="fa fa-caret-down"></i></button>
+    <div class="dropdown-content">
+      <a href="javascript:void(0)">Churrasqueira para Embutir a Carvão</a>
+      <a href="javascript:void(0)">Churrasqueira Portátil a Carvão</a>
+    </div>
+  </div>
+  <div class="dropdown">
+    <button href="javascript:void(0)" class="dropbtn">CHURRASQUEIRAS PROFISSIONAIS <i class="fa fa-caret-down"></i></button>
+    <div class="dropdown-content">
+      <a href="javascript:void(0)">Churrasqueira p/ Espetinho a Carvão para BalcãoChurrasqueira para Embutir a Carvão</a>
+      <a href="javascript:void(0)">Churrasqueira p/ Espetinho a Carvão Portátil</a>
+    </div>
+  </div>
+  <a href="javascript:void(0)">GRILL</a>
+  <div class="dropdown">
+    <button href="javascript:void(0)" class="dropbtn">ACESSÓRIOS <i class="fa fa-caret-down"></i></button>
+    <div class="dropdown-content">
+      <a href="javascript:void(0)">Grelha Argentina</a>
+      <a href="javascript:void(0)">Espeto</a>
+      <a href="javascript:void(0)">Grelha Moeda</a>
+      <a href="javascript:void(0)">Anti Chama</a>
+      <a href="javascript:void(0)">Pazinha Raspadora</a>
+      <a href="javascript:void(0)">Peças de Reposição</a>
+    </div>
+  </div>
+  <a href="javascript:void(0)">FOTOS E VIDEOS</a>
+  <a href="javascript:void(0)">EMPRESA</a>
+  <a href="javascript:void(0)">CONTATO</a>
+</div> -->
