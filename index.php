99d11e9f28c8bfb1198a751352728fd7b905a2a0
Guilherme Zanetti <guilherme.t.zanetti@gmail.com>
2021-01-16 08:03:01 -0300

    Alteração de tema da Navegação Produtos (Categorias)

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
   <link rel="stylesheet" href="frontend/css/cabecalho.css">
   <link rel="stylesheet" href="frontend/css/linhaDeChurrasqueiras.css">
   <link rel="stylesheet" href="frontend/css/navbarMobile.css">
+  <link rel="stylesheet" href="frontend/css/produtos.css">
 
   <!-- FONT-STYLE -->
   <link href="//fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic&amp;subset=latin,cyrillic" rel="stylesheet" type="text/css">
@@ -138,19 +139,99 @@
   </section>
 
   <!--    PRODUTOS    -->
-  <pre>
+  <section id="produtos">
+    <div id="container-nav-produtos">
+      <div class="body-navProdutos">
+        <nav class="accordion arrows">
+          <header class="box" id="categorias">
+            <label for="acc-close" class="box-title">CATEGORIAS</label>
+            <!-- <i class="fas fa-plus-circle" id="Cmais"></i> -->
+            <!-- <i class="fas fa-minus-circle" id="Cmenos"></i> -->
+          </header>
+          <input type="radio" name="accordion" id="cb-p1" />
+          <section class="box">
+            <label class="box-title" for="cb-p1">Churrasqueiras Residenciais</label>
+            <label class="box-close" for="acc-close"></label>
+            <div class="box-content container dropdown">
+              <div class="row">
+                <a href="javascript:void(0)" class="link-nvMobile col-12">Churrasqueiras para Embutir a Carvão</a>
+              </div>
+              <div class="row">
+                <a href="javascript:void(0)" class="link-nvMobile col-12">Churrasqueiras Portátil a Carvão</a>
+              </div>
+            </div>
+          </section>
+          <input type="radio" name="accordion" id="cb-p2" />
+          <section class="box">
+            <label class="box-title" for="cb-p2">Churrasqueiras Profissionais</label>
+            <label class="box-close" for="acc-close"></label>
+            <div class="box-content container dropdown">
+              <div class="row">
+                <a href="javascript:void(0)" class="link-nvMobile col-12">Churrasqueira p/ Espetinho a Carvão para Balcão</a>
+              </div>
+              <div class="row">
+                <a href="javascript:void(0)" class="link-nvMobile col-12">Churrasqueira p/ Espetinho a Carvão Portátil</a>
+              </div>
+            </div>
+          </section>
+          <input type="radio" name="accordion" id="cb-p3" />
+          <section class="box">
+            <a href="javascript:void(0)" class="box-title-ndrop">Grill Giratório</a>
+          </section>
+          <input type="radio" name="accordion" id="cb-p4" />
+          <section class="box">
+            <a href="javascript:void(0)"></a><label class="box-title" for="cb-p4">Acessórios</label></a>
+            <label class="box-close" for="acc-close"></label>
+            <div class="box-content container dropdown">
+              <div class="row">
+                <a href="javascript:voi(0)" class="link-nvMobile col-12">Grelha Argentina</a>
+              </div>
+              <div class="row">
+                <a href="javascript:voi(0)" class="link-nvMobile col-12">Espeto</a>
+              </div>
 
+              <div class="row">
+                <a href="javascript:voi(0)" class="link-nvMobile col-12">Grelha Moeda</a>
+              </div>
 
+              <div class="row">
+                <a href="javascript:voi(0)" class="link-nvMobile col-12">Anti Chama</a>
+              </div>
+              <div class="row">
+                <a href="javascript:voi(0)" class="link-nvMobile col-12">Pazinha Raspadora</a>
+              </div>
+              <div class="row">
+                <a href="javascript:voi(0)" class="link-nvMobile col-12">Peças de Reposição</a>
+              </div>
+            </div>
+          </section>
+          <input type="radio" name="accordion" id="cb-p5" />
+          <section class="box">
+            <a href="javascript:void(0)" class="box-title-ndrop">Fotos e Vídeos</a>
+          </section>
+          <input type="radio" name="accordion" id="cb-p6" />
+          <section class="box">
+            <a href="javascript:void(0)" class="box-title-ndrop">Catálago</a>
+          </section>
+          <input type="radio" name="accordion" id="cb-p7" />
+          <section class="box">
+            <a href="javascript:void(0)" class="box-title-ndrop">Manuais</a>
+          </section>
+          <input type="radio" name="accordion" id="cb-p8" />
+          <section class="box">
+            <a href="javascript:void(0)" class="box-title-ndrop">Empresa</a>
+          </section>
+          <input type="radio" name="accordion" id="cb-p9" />
+          <section class="box">
+            <a href="javascript:void(0)" class="box-title-ndrop">Contato</a>
+          </section>
 
+          <input type="radio" name="accordion" id="acc-close" />
+        </nav>
+      </div>
+    </div>
+  </section>
 
-PRODUTOS
-
-
-
-
-
-
-</pre>
   <!--   VIDEOS e REDES SOCIAIS   -->
   <section id="redesociais">
     <div class="container">
