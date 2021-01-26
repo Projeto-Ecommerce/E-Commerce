5ae1e18c5d2fa96ab9ddca4987442423f0a8e1cf
VinTesta <viniciustestapassos@gmail.com>
2021-01-26 13:46:56 -0300

    Verificação de CPF finalizada
    Já pode testar a parte de CPF

--- backend/functions/cadastroInit.php
+++ backend/functions/cadastroInit.php
@@ -30,46 +30,110 @@ if(isset($_POST['btn-cadastrar-first'])){
 
 //  VERIFICA CPF
 
-$cpf = preg_replace('/[^0-9]/',"",$_POST['cpf']);
-    
 
+    
+    $cpf = preg_replace('/[^0-9]/',"",$_POST['cpf']);    
     $email = htmlspecialchars($_POST['email']);
     $nome = clear($_POST['nome']);
     $celular = preg_replace('/[^0-9]/',"",$_POST['celular']);
     $ddd = '('.substr($celular, 0, 2).')';
     $celular = substr($celular, 2, 11);
-    $celular = $ddd.$celular;
+    $celular = $ddd.$celular;    
 
-    if (strlen($cpf) != 11) {
-        //  MENSAGEM DE ERRO POR CPF ERRADO
-        echo "CPF INVALIDO!";
-        return false;
-    }else{
-
-        $i = 0;
-        $soma = 0;
-        $multiplicador = 10;
-        while($i < 9){
+    if(substr($cpf, 0, 3) == substr($cpf, 3,3)){
+        // CPF INVALIDO
+        header('Location: erroCadastro.php?erro=1');
+    }else {
+        if (strlen($cpf) != 11) {
+            //  MENSAGEM DE ERRO POR CPF ERRADO
+            header('Location: erroCadastro.php?erro=1');
+            return false;
+        }else{
+    
+            $i = 0;
+            $soma = 0;
+            $multiplicador = 10;
+            while($i < 9){
+                $arrayMult[$i] = ($multiplicador * substr($cpf, $i, 1));
+                $multiplicador = $multiplicador - 1;     
+                $soma = $arrayMult[$i] + $soma;
+    
+                $i++;
+            }    
             
+            $verificaFirst = $soma / 11;
+            $verificaFirst = substr($verificaFirst, 2);
+            if(substr($verificaFirst, 0, 1) != '.'){
+                $verificaFirst = '0.'. $verificaFirst; 
+            }
             
+            $verificaFirst = round($verificaFirst * 11);
+            $verificaFirst = 11-$verificaFirst;
+
+            if($verificaFirst < 2){
+                $verificaFirst = 0;
+    
+            }
+    
+            $i = 0;
+            $soma = 0;
+            $multiplicador = 11;
+            while($i<10){
+                $arrayMult[$i] = ($multiplicador * substr($cpf, $i, 1));
+                $multiplicador = $multiplicador - 1;     
+                $soma = $arrayMult[$i] + $soma;
+                $i++;
+            }
+    
+            $verificaTwo = $soma / 11;
+            $verificaTwo = substr($verificaTwo, 2);
+            if(substr($verificaTwo, 0, 1) != "."){
+                $verificaTwo = '0.'. $verificaTwo; 
+            }
+            $verificaTwo = round($verificaTwo * 11);
+            $verificaTwo = 11-$verificaTwo;
+            if($verificaTwo > 9){
+                $verificaTwo = 0;
+    
+            }
+
+            $valorFinal = $verificaFirst.$verificaTwo;
+
+            $verificaValues = substr($cpf, 9,2);
             
+            if($verificaValues == $valorFinal){
 
-            echo '<br>'.$multiplicador.'<br>';
-            $arrayMult[$i] = ($multiplicador * substr($cpf, $i, 1));
-
-            $multiplicador = $multiplicador - 1;
-            echo $arrayMult[$i].'<br>';
-            $i++;
-            
-            $soma = $arrayMult[$i] + $soma;
-
-            echo "<br>$soma";
+                // header('Location: segundoCadastro.php?erro=1');
+            }
         }
-
         
     }
 
+
     
+
+    // echo "<br>$soma<br>"; 
+
+    // echo "<br>$verificaFirst<br>";
+
+    // echo "<br>$verificaTwo<br>";
+    
+    // echo "<br> $verificaValues<br>";
+
+    // echo "<br>$valorFinal<br>";
+?> 
+
+<?php
+
+    $filtraCpf[0] = substr($cpf, 0,3);
+
+    $filtraCpf[1] = substr($cpf, 3,3);
+
+    $filtraCpf[2] = substr($cpf, 6,3);
+
+    $filtraCpf[3] = substr($cpf, 9, 2);
+
+    $cpf = $filtraCpf[0].'.'.$filtraCpf[1].'.'.$filtraCpf[2].'-'. $filtraCpf[3];
 
     echo "<br> $cpf <br> $email <br> $nome <br> $celular <br> $ddd";
 }
