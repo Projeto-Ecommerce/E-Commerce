cece186bc5340c156eed850fc378f65dfd88f05c
VinTesta <viniciustestapassos@gmail.com>
2021-02-16 15:55:19 -0300

    Inicio cadastro de cliente

--- view/javascript/cadastroCliente.js
+++ view/javascript/cadastroCliente.js
@@ -39,8 +39,101 @@ $(document).ready(function(){
     }
   })
 
-  document.getElementById('nome').addEventListener('keyup', function() {
-    nome = document.getElementById('nome').value.split(" ");
-    console.log(typeof nome)
+  // >>> VALIDAÇÃO DO CAMPO NOME <<<
+  function validaNome(x) {
+    x = x.value.split(" ");
+    if(x[1] != undefined){
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  // >>> VALIDAÇÃO DO CAMPO DE CPF <<<
+  function validaCpf(x) {
+    cpf['length'] = x.value.length;
+    cpf['value'] = x.value;
+    if(cpf['length'] != 14){
+      return false;
+    }
+
+    cpf['clean'] = x.value.replace(/\.|\-/g, '');
+    // console.log(cpf['length']);
+    // console.log(cpf['value']);
+    // console.log(cpf['clean']);
+    var cpfArray = ['CPF'];
+    var soma = 0;
+    var mult = 0;
+    var i = 0;
+    var j = 10;
+    while(i <= 8){
+      mult = cpf['clean'].substring(i, i + 1) * j;     
+      soma = soma + mult;
+      i++;
+      j--
+    }
+
+    var firstNum = (soma * 10) % 11
+    if(firstNum >= 10){
+      firstNum = 0;
+    }
+
+    soma = 0
+    i = 0;
+    j = 11
+    while(i <= 9){
+      mult = cpf['clean'].substring(i, i + 1) * j;  
+      soma = soma + mult;
+      i++;
+      j--;
+      
+    }
+
+    var secondNum = (soma * 10) % 11
+    if(secondNum >= 10){
+      secondNum = 0;
+    }
+
+    finalNum = firstNum.toString() + secondNum.toString();
+     
+    if(finalNum == cpf['clean'].substring(9)){
+      return true;
+    }
+  }
+
+  // >>> VALIDAÇÃO DE EMAIL <<<
+
+  function validaEmail (x){
+    email['value'] = x.value;
+    email['length'] = x.value.length; 
+
+    if(email['length'] == undefined){
+      return false;
+    }
+    
+    email = email['value'].split(" ")
+    if(email['value'][1] != undefined){
+      return false;
+    }
+    
+  }
+  
+
+  // VALIDAÇÃO DOS CAMPOS
+  var nome = document.getElementById('nome')
+
+  var cpf = document.getElementById('cpf');
+
+  var email = document.getElementById('email');
+
+  email.addEventListener('keyup', function (){
+    console.log(validaEmail(email))
+  })
+
+  nome.addEventListener('keyup', function (){
+    console.log(validaNome(nome));
+    if(validaNome(nome) == false){
+      nome.style.background = 'rgb'
+    }
   })
 })
